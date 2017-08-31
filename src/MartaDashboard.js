607fa5aafa5732fc5d6551c4a1e1e7d8b0ba3738
Mac User <sabbey37@gmail.com>
2017-08-31 15:13:40 -0400

    Gets Marta train data from API

---
+++ src/MartaDashboard.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+const getMartaData = (cb) => {
+    fetch('http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=2c514350-0c26-47dd-b872-7936af81c8e1', {
+	method: 'get'
+}).then(function(response) {
+	return response.json()
+}).then(function(jsonData) {
+    console.log(jsonData);
+}).catch(function(err) {
+	// Error :(
+});
+}
+
+
+
+class MartaDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            martaData: []
+        };
+        this.componentWillMount = this.componentWillMount.bind(this);
+    }
+    
+    componentWillMount() {
+        this.martaDataGrabber = setInterval(() => {
+        getMartaData((jsonData) => {
+            this.setState({
+                martaData: jsonData
+            });
+        });
+        }, 10000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.martaDataGrabber);
+    }
+
+    render() {
+        let martaOutput = this.state.martaData.map((datum) => <p>{datum}</p>)
+        return (
+            <div>
+                <h2>Martaaaah</h2>
+                <div>
+                    {martaOutput}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MartaDashboard;
